143998413b6ec95a1dfd23095145117e6b5f3295
mlsoft <ljh0901@mlsoft.com>
2023-05-02 14:58:23 +0900

    UPDATE : Quiz1Test 품

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -3,28 +3,54 @@ package com.mangkyu.stream.Quiz1;
 import com.opencsv.CSVReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Quiz1 {
 
     // 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz1() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+        return csvLines.stream()
+                .flatMap(line -> Arrays.stream(line[1].split(":")))
+                .map(String::trim)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(e -> 1)));
+
     }
 
     // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz2() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+        return csvLines.stream()
+                .filter(line -> line[0].startsWith("정"))
+                .flatMap(line -> Arrays.stream(line[1].split(":")))
+                .map(String::trim)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(e -> 1)));
     }
 
     // 1.3 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
     public int quiz3() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return 0;
+        return csvLines.stream().map(line -> countJoa(line[2])).reduce(Integer::sum).get();
+    }
+
+    private int countJoa(String s) {
+        int result = 0;
+        int start = 0;
+        while (true) {
+            int getJoa = s.indexOf("좋아", start);
+            if (getJoa != -1) {
+                result++;
+                start = getJoa + 2;
+            } else {
+                break;
+            }
+        }
+        return result;
     }
 
     private List<String[]> readCsvLines() throws IOException {
--- src/test/java/com/mangkyu/stream/Quiz1/Quiz1Test.java
+++ src/test/java/com/mangkyu/stream/Quiz1/Quiz1Test.java
@@ -1,9 +1,15 @@
 package com.mangkyu.stream.Quiz1;
 
 import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Quiz1Test {
 
@@ -23,6 +29,25 @@ class Quiz1Test {
     @Test
     void quiz3() throws IOException {
         assertThat(quiz.quiz3()).isEqualTo(answer.quiz3());
+    }
+
+    @Test
+    void wordProcessTest() {
+        List<String> words = Arrays.asList("TONY", "a", "hULK", "B", "america", "X", "nebula", "Korea");
+
+        String result = words.stream()
+                .filter(w -> w.length() > 1)
+                .map(String::toUpperCase)
+                .map(w -> w.substring(0, 1))
+                .collect(Collectors.joining(" "));
+
+        assertEquals(result, "T H A N K");
+    }
+
+    @Test
+    void customFunction() {
+        Function<String, String> function = word -> word.toUpperCase();
+        assertEquals(function.apply("text"), "TEXT");
     }
 
 }
